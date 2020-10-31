32f0d1740a71b11ee6513ae5fdf800b2dc2dd78f
mehrab-haque <mehrab.haque.0001@gmail.com>
2020-10-31 09:58:15 +0600

    created debug logs

--- app.js
+++ app.js
@@ -73,16 +73,18 @@ app.post('/bkash_production',function(req,res){
         var org=msg_json["creditOrganizationName"];
         var timestamp=msg_json["dateTime"];
         var trxId=msg_json["trxID"];
+        console.log("payment recieved")
         if(ref.split(CODE_DELIMITER).length==4){
           var arr=ref.split(CODE_DELIMITER)
           if(arr[0] in products_schema){
             axios.post(products_schema[arr[0]],{
               code:ref
             }).then(result=>{
+              console.log("posted to product endpoint")
               con1.query("INSERT INTO payment_logs (org,trxID,sender,amount,ref,timestamp) VALUES (?,?,?,?,?,?)", [org,trxId,sender,amount,ref,timestamp], function(error, results, fields) {
                 if (error){
-                  res.end("error");
                   console.log("mysql error");
+                  res.end("error");
                 }else{
                   console.log("db done");
                   res.end("ok");
@@ -92,9 +94,11 @@ app.post('/bkash_production',function(req,res){
               res.end("error")
             })
           }else{
+            console.log("product not found")
             res.end("error")
           }
         }else{
+          console.log("code error")
           res.end("error");
         }
       }
